1c21c7fc8ee733aabb1fca8c1e60e79bb9daff92
mspielberg <9729801+mspielberg@users.noreply.github.com>
2018-07-22 23:28:08 -0700

    add graph library

---
+++ Graph.lua
@@ -0,0 +1,58 @@
+local Graph = {}
+
+function Graph.new()
+  local self = {}
+  return setmetatable(self, { __index = Graph })
+end
+
+-- add node with neighbors ...
+function Graph:add(node, ...)
+  local neighbors = {...}
+  if not self[node] then self[node] = {} end
+  for _, neighbor in ipairs(neighbors) do
+    if not self[neighbor] then self[neighbor] = {} end
+    self[node][neighbor] = true
+    self[neighbor][node] = true
+  end
+end
+
+function Graph:remove(node)
+  if not self[node] then return end
+  for neighbor in pairs(self[node]) do
+    self[neighbor][node] = nil
+  end
+  self[node] = nil
+end
+
+local function dfs(self, node, visited)
+  visited[node] = true
+  for neighbor in pairs(self[node]) do
+    if not visited[neighbor] then
+      dfs(self, neighbor, visited)
+    end
+  end
+  return visited
+end
+
+function Graph:removal_fragments(node)
+  local original_neighbors = {}
+  for neighbor in pairs(self[node]) do
+    original_neighbors[neighbor] = true
+  end
+
+  local fragments = {}
+  for start_point in pairs(self[node]) do
+    local fragment = dfs(self, start_point, {[node] = true})
+    for neighbor in pairs(self[node]) do
+      if fragment[neighbor] then
+        self[node][neighbor] = nil
+      end
+    end
+    fragment[node] = nil
+    fragments[#fragments+1] = fragment
+  end
+  self[node] = original_neighbors
+  return fragments
+end
+
+return Graph
