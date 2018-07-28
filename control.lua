28c4855a00f8a5cd8b522769a6cd92c1c563dc32
mspielberg <9729801+mspielberg@users.noreply.github.com>
2018-07-28 10:50:24 -0700

    refactor event registration

--- control.lua
+++ control.lua
@@ -9,34 +9,54 @@ local function on_init()
 end
 
 local function on_load()
+  Blueprint.on_load()
   Editor.on_load()
   Network.on_load()
 end
 
-local function on_built_entity(event)
-  if event.mod_name then
-    Editor.on_robot_built_entity(event)
-  else
-    Blueprint.on_player_built_entity(event)
-    Editor.on_player_built_entity(event)
-  end
-end
+local event_handlers = {
+  on_built_entity = function(event)
+    if event.mod_name then
+      Editor.on_robot_built_entity(event)
+    else
+      Blueprint.on_player_built_entity(event)
+      Editor.on_player_built_entity(event)
+    end
+  end,
 
-local function on_robot_built_entity(event)
-  Editor.on_robot_built_entity(event.robot, event.created_entity, event.stack)
-end
+  on_robot_built_entity = function(event)
+    Editor.on_robot_built_entity(event.robot, event.created_entity, event.stack)
+  end,
 
-local function on_pre_player_mined_item(event)
-  Blueprint.on_pre_player_mined_item(event.player_index, event.entity)
-end
+  on_pre_player_mined_item = function(event)
+    Blueprint.on_pre_player_mined_item(event.player_index, event.entity)
+  end,
 
-local function on_pre_ghost_deconstructed(event)
-  Blueprint.on_pre_player_mined_item(event.player_index, event.ghost)
-end
+  on_player_mined_entity = function(event)
+    Blueprint.on_player_mined_entity(event.player_index, event.entity, event.buffer)
+    Editor.on_player_mined_entity(event)
+  end,
 
-local function on_player_setup_blueprint(event)
-  Blueprint.on_player_setup_blueprint(event)
-end
+  on_pre_ghost_deconstructed = function(event)
+    Blueprint.on_pre_player_mined_item(event.player_index, event.ghost)
+  end,
+
+  on_player_setup_blueprint = function(event)
+    Blueprint.on_player_setup_blueprint(event)
+  end,
+
+  on_put_item = function(event)
+    Blueprint.on_put_item(event)
+  end,
+
+  on_player_rotated_entity = function(event)
+     Editor.on_player_rotated_entity(event)
+  end,
+
+  on_entity_died = function(event)
+    Editor.on_entity_died(event)
+  end,
+}
 
 local function on_toggle_editor(event)
   Editor.toggle_editor_status_for_player(event.player_index)
@@ -49,16 +69,8 @@ end
 
 script.on_init(on_init)
 script.on_load(on_load)
-
-script.on_event(defines.events.on_built_entity, on_built_entity)
-script.on_event(defines.events.on_robot_built_entity, on_robot_built_entity)
-script.on_event(defines.events.on_pre_player_mined_item, on_pre_player_mined_item)
-script.on_event(defines.events.on_pre_ghost_deconstructed, on_pre_ghost_deconstructed)
-script.on_event(defines.events.on_player_mined_entity, Editor.on_player_mined_entity)
-script.on_event(defines.events.on_player_rotated_entity, Editor.on_player_rotated_entity)
-script.on_event(defines.events.on_entity_died, Editor.on_entity_died)
-script.on_event("plumbing-toggle-editor-view", on_toggle_editor)
-script.on_event(defines.events.on_player_setup_blueprint, on_player_setup_blueprint)
-script.on_event(defines.events.on_put_item, Blueprint.on_put_item)
-
 script.on_nth_tick(60, on_tick)
+script.on_event("plumbing-toggle-editor-view", on_toggle_editor)
+for event_name, handler in pairs(event_handlers) do
+  script.on_event(defines.events[event_name], handler)
+end
