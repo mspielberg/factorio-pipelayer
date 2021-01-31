33adcc12464ee5f3ba98a50d8af10293d97e71d1
mspielberg <9729801+mspielberg@users.noreply.github.com>
2021-01-30 20:42:58 -0800

    Handle Picker Belt Tools reviving ghosts on the editor surface Add migration to clean up orphan pipes without networks.

--- configchange.lua
+++ configchange.lua
@@ -1,3 +1,5 @@
+local Editor = require "Editor"
+local Network = require "Network"
 local version = require "lualib.version"
 
 local M = {}
@@ -17,5 +19,33 @@ function M.on_mod_version_changed(old)
     end
   end
 end
+
+add_migration{
+  name = "remove_orphan_underground_pipes",
+  task = function()
+    Editor.restore(global.editor)
+    local editor = global.editor
+    local entities_deleted = 0
+    local surfaces_affected = 0
+    for _, surface in pairs(game.surfaces) do
+      local surface_affected = false
+      if editor:is_editor_surface(surface) then
+        for _, entity in pairs(surface.find_entities()) do
+          if not Network.for_entity(entity) then
+            entity.destroy()
+            entities_deleted = entities_deleted + 1
+            surface_affected = true
+          end
+        end
+      end
+      if surface_affected then
+        surfaces_affected = surfaces_affected + 1
+      end
+    end
+    if entities_deleted > 0 then
+      log("Deleted "..entities_deleted.."orphaned entities on "..surfaces_affected.." surface(s).")
+    end
+  end,
+}
 
 return M
